a9546fd2d52722629c4c2c023fc58ea20dbe354d
Sanjeev kumar <sachinsaini19970@gmail.com>
2022-03-12 20:22:54 +0530

    auth system

--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,7 @@
 const homeController = require('../app/http/controllers/homeController');
 const authController = require('../app/http/controllers/authController');
 const cartController = require('../app/http/controllers/customers/cartController');
+const guest = require('../app/http/middleware/guest');
 
 function initRoutes(app) {
     app.get('/', homeController().index);
@@ -8,9 +9,14 @@ function initRoutes(app) {
     app.get('/cart', cartController().index);
     app.post('/update-cart', cartController().update)
     
-    app.get('/login', authController().login);
+    app.get('/login', guest, authController().login);
+    app.post('/login', authController().postLogin);
     
-    app.get('/register', authController().register);
-}
+    app.get('/register', guest, authController().register);
+    app.post('/register', authController().postRegister)
+
+    app.post('/logout', authController().logout)
+
+};
 
 module.exports = initRoutes;
--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const session = require('express-session')
 const flash = require('express-flash');
-const MongoDbStore = require('connect-mongo')
+const MongoDbStore = require('connect-mongo');
+const passport = require('passport');
 
 // Database connection
 const url = 'mongodb://localhost:27017/Pizza';
@@ -19,30 +20,46 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
     console.log(error);
 });
 
-//Session store 
-let mongoStore = new MongoDbStore({
-    mongoUrl: 'mongodb://localhost:27017/Pizza',
-    collection: 'sessions'
-  });
+
+
+// Session store  // old Version 
+// let mongoStore = new MongoDbStore({
+//     mongoUrl: 'mongodb://localhost:27017/Pizza',
+//     collection: 'sessions'
+//   });
+
 
 // Session config
 app.use(session({
     secret: process.env.COOKIE_SECRET,
     resave: false,
-    store: mongoStore,
+    store: MongoDbStore.create({
+        mongoUrl: 'mongodb://localhost:27017/Pizza',
+        // collection: 'sessions'
+    }),
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 Hours
 }))
 
+// Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash())
+
 
 // Assets
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 
 // Global middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 
